01ab31c3746942cfc18a5a7091ac5ddd05400f40
jorgt <jorg.thuijls@gmail.com>
2022-05-28 12:56:22 +1000

    build

---
+++ app/dist/Component-dbg.js
@@ -0,0 +1,41 @@
+/* eslint-disable no-undef */
+
+sap.ui.define(["sap/ui/core/UIComponent", "sap/ui/core/EventBus"], function (UIComponent) {
+  "use strict";
+
+  return UIComponent.extend("dalrae.cap.supabase.fioridemo.Component", {
+    metadata: {
+      manifest: "json",
+    },
+
+    /**
+     * The component is initialized by UI5 automatically during the startup of the app and calls the init method once.
+     * In this function, the FLP and device models are set and the router is initialized.
+     * @public
+     * @override
+     */
+    init: function () {
+      // call the base component's init function
+      UIComponent.prototype.init.apply(this, arguments);
+    },
+
+    /**
+     * The component is destroyed by UI5 automatically.
+     * In this method, the ErrorHandler is destroyed.
+     * @public
+     * @override
+     */
+    destroy: function () {},
+
+    /**
+     * This method can be called to determine whether the sapUiSizeCompact or sapUiSizeCozy
+     * design mode class should be set, which influences the size appearance of some controls.
+     * @public
+     * @return {string} css class, either 'sapUiSizeCompact' or 'sapUiSizeCozy' - or an empty string if no css class should be set
+     */
+    getContentDensityClass: function () {
+      this._sContentDensityClass = "sapUiSizeCompact";
+      return this._sContentDensityClass;
+    },
+  });
+});
