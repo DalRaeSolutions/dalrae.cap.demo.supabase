b4a4dcff81f9622ab8e82eda07e42cdcdcb2b102
jorgt <jorg.thuijls@gmail.com>
2022-06-01 21:14:01 +1000

    index file etc

--- libs/auth.js
+++ libs/auth.js
@@ -1,13 +1,17 @@
+/* eslint-disable no-unused-vars */
 const cds = require('@sap/cds');
 
 module.exports = (req, res, next) => {
-  if (res?.locals?.user) {
-    const { email } = res.locals.user;
-    console.log('[auth] logged in as:', email);
-    req.user = new cds.User({ ...res.locals.user, id: email });
-    next()
-  } else {
-    console.log('[auth]', !!res.locals.user)
-    res.status(401).end('No user info');
-  }
+
+  res.status(401).end('No user info');
+  
+  // if (res?.locals?.user) {
+  //   const { email } = res.locals.user;
+  //   console.log('[auth] logged in as:', email);
+  //   req.user = new cds.User({ ...res.locals.user, id: email });
+  //   next()
+  // } else {
+  //   console.log('[auth]', !!res.locals.user)
+  //   res.status(401).end('No user info');
+  // }
 }
--- srv/serverImplementation.js
+++ srv/serverImplementation.js
@@ -6,22 +6,43 @@ const supabase = createClient('http://localhost:59921', 'eyJhbGciOiJIUzI1NiIsInR
 
 module.exports = async (app) => {
   app.use(express.json());
-  app.use(cookieParser());
-  app.use(async ({cookies, ...req}, res, next) => {
-    const token = cookies['sb-access-token'];
-    const {data: user, error } = await supabase.auth.api.getUser(token);
-    res.locals = { user, error };
 
-    next();
-  })
+  /**
+   * 1. Enable cookie parser
+   */
+  //app.use(cookieParser());
 
-  app.get('/auth/me', (req, res) => {
-    res.status(200).json(res?.locals?.user)
-  });
 
-  app.get('/auth/cookies', (req, res) => {
-    res.clearCookie('sb-access-token', { domain: 'localhost', httpOnly: true, secure: true });
-    res.clearCookie('sb-refresh-token', { domain: 'localhost', httpOnly: true, secure: true });
-    res.status(200).json({success: true})
-  });
+  /**
+   * 2. Show cookie output
+   */
+  // app.use(async ({ cookies, ...req }, res, next) => {
+  //   if (!cookies) return next();
+
+  //   const token = cookies['sb-access-token'];
+  //   console.log(token);
+
+  //   next();
+  // })
+
+  /**
+   * Validate cookie and pass the results on to other middleware
+   */
+  // app.use(async ({cookies, ...req}, res, next) => {
+  //   const token = cookies['sb-access-token'];
+  //   const {data: user, error } = await supabase.auth.api.getUser(token);
+  //   res.locals = { user, error };
+
+  //   next();
+  // })
+
+  // app.get('/auth/me', (req, res) => {
+  //   res.status(200).json(res?.locals?.user)
+  // });
+
+  // app.get('/auth/cookies', (req, res) => {
+  //   res.clearCookie('sb-access-token', { domain: 'localhost', httpOnly: true, secure: true });
+  //   res.clearCookie('sb-refresh-token', { domain: 'localhost', httpOnly: true, secure: true });
+  //   res.status(200).json({success: true})
+  // });
 }
